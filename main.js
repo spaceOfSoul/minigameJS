6ffe2e45014450c30a081286013bf0bf83960ed1
qwer <85132981+ch-tuna@users.noreply.github.com>
2022-05-25 00:26:49 +0900

    enemy

--- main.js
+++ main.js
@@ -46,13 +46,65 @@ class Projectile{
     }
 }
 
+class Enemy{
+    constructor(x,y,radius,color, velocity){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+    }
+
+    draw(){
+        c.beginPath();
+        //x,y,r,startAngle, endAngle, drawCounterClockwise
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false );
+        c.fillStyle = this.color;
+        c.fill();
+    }
+
+    update(){
+        this.draw();
+        this.x = this.x + this.velocity.x;
+        this.y = this.y + this.velocity.y;
+    }
+}
+
 const x = canvas.width / 2;
 const y = canvas.height / 2;
-let isDrawing = false;
+let enemyCount = 1000;
 
 const player = new Player(x, y, 30, 'blue');
 
 const projectiles = [];
+const enemies = [];
+
+function spawnEnemies(){
+    setInterval(()=>{
+        let minimum = 15;
+        const radius = Math.random() * (30 - minimum) + minimum;
+        let x;
+        let y;
+
+        if(Math.random() < 0.5){
+            x = Math.random() < 0.5 ? 0-radius : canvas.width + radius;
+            y = Math.random() * canvas.height;
+        }else{
+            x = Math.random() *canvas.width;
+            y = Math.random() < 0.5 ? 0-radius : canvas.height + radius;
+        }
+
+
+        const color = 'purple';
+        const angle = Math.atan2(canvas.height/2-y,canvas.width/2-x);
+        const velocity ={
+            x:Math.cos(angle),
+            y:Math.sin(angle)
+        };
+
+        enemies.push(new Enemy(x,y,radius,color,velocity));
+    },enemyCount);
+}
 
 function animate(){
     requestAnimationFrame(animate);
@@ -60,6 +112,10 @@ function animate(){
     player.draw();
     projectiles.forEach(projectile =>{
         projectile.update();
+    });
+
+    enemies.forEach(enemy =>{
+        enemy.update();
     });
 }
 
@@ -72,4 +128,5 @@ addEventListener('mousedown',(e)=>{
     projectiles.push(new Projectile( canvas.width/2, canvas.height/2, 5, 'red', velocity))
 });
 
+spawnEnemies();
 animate();
