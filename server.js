a73811ce1b5d19ab492a3dc1bbb34685249d0ff3
qwer <85132981+ch-tuna@users.noreply.github.com>
2022-06-17 18:46:12 +0900

    public folder

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
 const express = require('express');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
+
+const http = require('http').createServer(app);
+
+require('dotenv').config();
+
+app.listen(3000, ()=>{
+    console.log("server start!");
+});
+app.set('view engine','ejs');
+
+app.get('/', function (req, res) {
+    res.render('index.ejs');
+  });
